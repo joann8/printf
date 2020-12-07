bda3c2ef72b679a37b0745ae05b6cdd9cd202b14
joann8 <joann.acher8@gmail.com>
2020-12-07 17:17:43 +0100

    char test

--- v2/srcs/ft_char.c
+++ v2/srcs/ft_char.c
@@ -6,7 +6,7 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 14:28:47 by jacher            #+#    #+#             */
-/*   Updated: 2020/12/07 17:11:27 by jacher           ###   ########.fr       */
+/*   Updated: 2020/12/07 17:17:31 by jacher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,27 +64,17 @@ int		ft_char(va_list args, int *res, flag_list *flags)
 		size--;
 	if (!(c = malloc(sizeof(char) * (size + 1))))
 		return ((*res = -1)); //erreur
-	if (flags->b_flag_minus == 0)
-	{
-		if (car == '\0')
-		{		
-			ft_char_right(' ', c, size, 1);
-			write(1, "\0", 1);
-		}
-		else
-			ft_char_right(car, c, size, 1);	
-	}
-	else
-	{
-		if (car == '\0')
-		{		
-			write(1, "\0", 1);
-			ft_char_right(' ', c, size, 1);
-		}
-		else
-			ft_char_left(car, c, size);
-	}
+	if (car == '\0')
+		ft_char_right(' ', c, size, 1);
+	else if (flags->b_flag_minus == 0)
+		ft_char_right(car, c, size, 1);	
+	else 
+		ft_char_left(car, c, size);
+	if (car == '\0' && flags->b_flag_minus == 1)
+		write(1, "\0", 1);
 	ft_putstr(c);
+	if (car == '\0' && flags->b_flag_minus == 0)
+		write(1, "\0", 1);
 	*res += ft_strlen(c);
 	if (car == '\0')
 		*res += 1;
