10154c6f6800f7db20bd41bbb629bb326055f8b2
joann8 <joann.acher8@gmail.com>
2020-12-08 19:13:13 +0100

    string null

--- v2/srcs/ft_string.c
+++ v2/srcs/ft_string.c
@@ -6,7 +6,7 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 19:18:21 by jacher            #+#    #+#             */
-/*   Updated: 2020/12/08 19:07:18 by jacher           ###   ########.fr       */
+/*   Updated: 2020/12/08 19:13:00 by jacher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ int		ft_string(va_list args, int *res, flag_list *flags)
 	if (s == NULL)
 	{
 		s = ft_strdup("(null)");
-		if (flags->b_width == 0 && flags->b_star_width == 1)
+		if (flags->b_width == 0 && flags->b_star_length == 1)
 			flags->b_precision = 0;
 	}
 	width = ft_strlen(s);
