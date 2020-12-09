2d3b0db2c66424a096759615b6a3e01971ac8b0c
joann8 <joann.acher8@gmail.com>
2020-12-09 17:22:35 +0100

    norme char

--- v2/srcs/ft_char.c
+++ v2/srcs/ft_char.c
@@ -6,16 +6,16 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 14:28:47 by jacher            #+#    #+#             */
-/*   Updated: 2020/12/09 17:19:10 by jacher           ###   ########.fr       */
+/*   Updated: 2020/12/09 17:22:20 by jacher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../printf.h"
 
-void		ft_char_right(char car, char *c, unsigned int width, int mod)
+static void	ft_char_right(char car, char *c, unsigned int width, int mod)
 {
-	unsigned int	i;
-	char			fill;
+	unsigned int i;
+	char		 fill;
 
 	if (mod == 1)
 		fill = ' ';
@@ -25,13 +25,13 @@ void		ft_char_right(char car, char *c, unsigned int width, int mod)
 	c[width] = '\0';
 	c[width - 1] = car;
 	while (i < width - 1)
-	{
+	{	
 		c[i] = fill;
 		i++;
 	}
 }
 
-void		ft_char_left(char car, char *c, unsigned int width)
+static void	ft_char_left(char car, char *c, unsigned int width)
 {
 	unsigned int i;
 
@@ -39,50 +39,38 @@ void		ft_char_left(char car, char *c, unsigned int width)
 	c[width] = '\0';
 	c[0] = car;
 	while (i + 1 < width)
-	{
+	{	
 		c[i + 1] = ' ';
 		i++;
 	}
 }
-
-int			ft_char_flag(flag_list *flags, unsigned int *size, va_list args, char *car)
+	
+int		ft_char(va_list args, int *res, flag_list *flags)
 {
-	if (flags->b_flag_zero == 1 || flags->b_precision == 1)
-		return (-1);
+	char			car;
+	char 			*c;
+	unsigned int	size;
+	
+	size = 1;
+	c = NULL;
+	if(flags->b_flag_zero == 1 || flags->b_precision == 1)
+		return ((*res = -1));
 	if (flags->b_width == 1 && flags->v_width > 1)
-		*size = flags->v_width;
-	*car = (char)va_arg(args, int);
-	if (*car == '\0')
+		size = flags->v_width;
+	car = (char)va_arg(args, int);
+	if (car == '\0')
 		size--;
-	return (1);
-}
-
-void		display_char(char car, flag_list *flags, unsigned int size, char *c)
-{
+	if (size > 0 && !(c = malloc(sizeof(char) * (size + 1))))
+		return ((*res = -1)); //erreur
 	if (car == '\0')
 	{
 		if (size > 0)
 			ft_char_right(' ', c, size, 1);
 	}
 	else if (flags->b_flag_minus == 0)
-		ft_char_right(car, c, size, 1);
-	else
+		ft_char_right(car, c, size, 1);	
+	else 
 		ft_char_left(car, c, size);
-}
-
-int			ft_char(va_list args, int *res, flag_list *flags)
-{
-	char			car;
-	char			*c;
-	unsigned int	size;
-
-	size = 1;
-	c = NULL;
-	if (ft_char_flag(flags, &size, args, &car) == -1)
-		return ((*res = -1));
-	if (size > 0 && !(c = malloc(sizeof(char) * (size + 1))))
-		return ((*res = -1));
-	display_char(car, flags, size, c);
 	if (car == '\0' && flags->b_flag_minus == 1)
 		write(1, "\0", 1);
 	if (size != 0)
@@ -98,27 +86,23 @@ int			ft_char(va_list args, int *res, flag_list *flags)
 	return (1);
 }
 
-int			ft_percent_flag(flag_list *flags, unsigned int *size)
-{
-	if (flags->b_precision == 1)
-		return (-1);
-	if (flags->b_width == 1 && flags->v_width > 1)
-		*size = flags->v_width;
-	return (1);
-}
-
-int			ft_percent(va_list args, int *res, flag_list *flags)
+int		ft_percent(va_list args, int *res, flag_list *flags)
 {
 	char			car;
 	char			*c;
 	unsigned int	size;
-
+	
 	(void)args;
 	size = 1;
-	if ((ft_percent_flag(flags, &size) == -1))
+	if(flags->b_precision == 1)
 		return ((*res = -1));
+	if (flags->b_width == 1 && flags->v_width > 1)
+		size = flags->v_width;
 	if (!(c = malloc(sizeof(char) * (size + 1))))
-		return ((*res = -1));
+	{
+		*res = -1;
+		return (-1); //erreur
+	}
 	car = '%';
 	if (flags->b_flag_minus == 0)
 	{
@@ -126,9 +110,9 @@ int			ft_percent(va_list args, int *res, flag_list *flags)
 			ft_char_right(car, c, size, 0);
 		else
 			ft_char_right(car, c, size, 1);
-	}
+	}	
 	else
-		ft_char_left(car, c, size);
+		ft_char_left(car, c, size);	
 	ft_putstr(c);
 	*res += ft_strlen(c);
 	free(c);
