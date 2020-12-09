05e40a6e25b3c75e29de8ead83ff3ac22f00ad76
joann8 <joann.acher8@gmail.com>
2020-12-09 17:53:06 +0100

    norme char

--- v2/srcs/ft_char.c
+++ v2/srcs/ft_char.c
@@ -6,7 +6,7 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 14:28:47 by jacher            #+#    #+#             */
-/*   Updated: 2020/12/09 17:46:43 by jacher           ###   ########.fr       */
+/*   Updated: 2020/12/09 17:52:53 by jacher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	ft_char_right(char car, char *c, unsigned int width, int mod)
 {
-	unsigned int i;
-	char		 fill;
+	unsigned int	i;
+	char			fill;
 
 	if (mod == 1)
 		fill = ' ';
@@ -25,7 +25,7 @@ void	ft_char_right(char car, char *c, unsigned int width, int mod)
 	c[width] = '\0';
 	c[width - 1] = car;
 	while (i < width - 1)
-	{	
+	{
 		c[i] = fill;
 		i++;
 	}
@@ -39,7 +39,7 @@ void	ft_char_left(char car, char *c, unsigned int width)
 	c[width] = '\0';
 	c[0] = car;
 	while (i + 1 < width)
-	{	
+	{
 		c[i + 1] = ' ';
 		i++;
 	}
@@ -53,37 +53,44 @@ void	display_char(char car, unsigned int size, flag_list *flags, char *c)
 			ft_char_right(' ', c, size, 1);
 	}
 	else if (flags->b_flag_minus == 0)
-		ft_char_right(car, c, size, 1);	
-	else 
+		ft_char_right(car, c, size, 1);
+	else
 		ft_char_left(car, c, size);
+}
+
+void	display_c(char *c, int *res)
+{
+	ft_putstr(c);
+	*res += ft_strlen(c);
+	free(c);
 }
 
 int		ft_char(va_list args, int *res, flag_list *flags)
 {
 	char			car;
-	char 			*c;
+	char			*c;
 	unsigned int	size;
-	
+
 	size = 1;
 	c = NULL;
-	if(flags->b_flag_zero == 1 || flags->b_precision == 1)
+	if (flags->b_flag_zero == 1 || flags->b_precision == 1)
 		return ((*res = -1));
 	if (flags->b_width == 1 && flags->v_width > 1)
 		size = flags->v_width;
-//	car = (char)va_arg(args, int);
 	if ((car = (char)va_arg(args, int)) == '\0')
 		size--;
 	if (size > 0 && !(c = malloc(sizeof(char) * (size + 1))))
-		return ((*res = -1)); 
+		return ((*res = -1));
 	display_char(car, size, flags, c);
 	if (car == '\0' && flags->b_flag_minus == 1)
 		write(1, "\0", 1);
 	if (size != 0)
-	{
+		display_c(c, res);
+	/*{
 		ft_putstr(c);
 		*res += ft_strlen(c);
 		free(c);
-	}
+	}*/
 	if (car == '\0' && flags->b_flag_minus == 0)
 		write(1, "\0", 1);
 	if (car == '\0')
