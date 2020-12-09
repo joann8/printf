a2b43c793371c5d0ec248979540416451852644f
joann8 <joann.acher8@gmail.com>
2020-12-09 18:09:25 +0100

    norme string

--- v2/srcs/ft_string.c
+++ v2/srcs/ft_string.c
@@ -6,7 +6,7 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 19:18:21 by jacher            #+#    #+#             */
-/*   Updated: 2020/12/09 18:02:23 by jacher           ###   ########.fr       */
+/*   Updated: 2020/12/09 18:09:04 by jacher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,8 @@ static void		ft_string_right(char *s, char *tmp, unsigned int width,
 	}
 }
 
-void	string_help(flag_list *flags, unsigned int *width, unsigned int *length, char *s)
+void	string_help(flag_list *flags, unsigned int *width, unsigned int *length,
+						char *s)
 {
 	*width = ft_strlen(s);
 	if (flags->b_width == 1)
@@ -77,51 +78,45 @@ void	string_help(flag_list *flags, unsigned int *width, unsigned int *length, ch
 	}
 }
 
+void	display_string(char *tmp, int *res, char *s, int bol)
+{
+	ft_putstr(tmp);
+	*res = ft_strlen(tmp);
+	free(tmp);
+	if (bol == 1)
+		free(s); 
+}
+
 int				ft_string(va_list args, int *res, flag_list *flags)
 {
 	char			*s;
 	char			*tmp;
 	unsigned int	length;
 	unsigned int	width;
+	int				bol;
 
+	bol = 0;
 	if (flags->b_flag_zero == 1)
 		return ((*res = -1));
 	s = va_arg(args, char *);
 	if (s == NULL)
 	{
 		s = ft_strdup("(null)");
+		bol = 1;
 		if (flags->b_width == 0 && flags->b_star_length == 1)
 			flags->b_precision = 0;
 	}
 	string_help(flags, &width, &length, s);
-/*	width = ft_strlen(s);
-	if (flags->b_width == 1)
-		width = flags->v_width;
-	length = ft_strlen(s);
-	if (flags->b_precision == 1)
-	{
-		length = flags->v_length;
-		if (length > ft_strlen(s))
-		{
-			if (width < ft_strlen(s))
-				width = ft_strlen(s);
-		}
-		if (length < ft_strlen(s))
-			if (flags->b_width == 0)
-				width = length;
-	}*/
 	if (!(tmp = malloc(sizeof(char) * (width + 1))))
 	{
-		//free(s);
+		if (bol == 1)
+			free(s);
 		return ((*res = 1));
 	}
 	if (flags->b_flag_minus == 1)
 		ft_string_left(s, tmp, width, length);
 	else
 		ft_string_right(s, tmp, width, length);
-	ft_putstr(tmp);
-	*res = ft_strlen(tmp);
-	free(tmp);
-//	free(s); //probleme leak
+	display_string(tmp, res, s, bol);
 	return (1);
 }
