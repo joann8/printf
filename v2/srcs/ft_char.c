74b6e00040d37a05620dacd060a899826cc93734
joann8 <joann.acher8@gmail.com>
2020-12-09 17:24:44 +0100

    norme char

--- v2/srcs/ft_char.c
+++ v2/srcs/ft_char.c
@@ -6,7 +6,7 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 14:28:47 by jacher            #+#    #+#             */
-/*   Updated: 2020/12/09 17:22:20 by jacher           ###   ########.fr       */
+/*   Updated: 2020/12/09 17:24:35 by jacher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,20 @@ static void	ft_char_left(char car, char *c, unsigned int width)
 		i++;
 	}
 }
-	
+
+void	display_char(char car, unsigned int size, flag_list *flags, char *c)
+{
+	if (car == '\0')
+	{
+		if (size > 0)
+			ft_char_right(' ', c, size, 1);
+	}
+	else if (flags->b_flag_minus == 0)
+		ft_char_right(car, c, size, 1);	
+	else 
+		ft_char_left(car, c, size);
+}
+
 int		ft_char(va_list args, int *res, flag_list *flags)
 {
 	char			car;
@@ -62,7 +75,7 @@ int		ft_char(va_list args, int *res, flag_list *flags)
 		size--;
 	if (size > 0 && !(c = malloc(sizeof(char) * (size + 1))))
 		return ((*res = -1)); //erreur
-	if (car == '\0')
+	/*if (car == '\0')
 	{
 		if (size > 0)
 			ft_char_right(' ', c, size, 1);
@@ -70,7 +83,7 @@ int		ft_char(va_list args, int *res, flag_list *flags)
 	else if (flags->b_flag_minus == 0)
 		ft_char_right(car, c, size, 1);	
 	else 
-		ft_char_left(car, c, size);
+		ft_char_left(car, c, size);*/
 	if (car == '\0' && flags->b_flag_minus == 1)
 		write(1, "\0", 1);
 	if (size != 0)
