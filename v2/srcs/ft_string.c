8da316605c6e6125149204dc55b949f88037798d
joann8 <joann.acher8@gmail.com>
2020-12-09 18:14:39 +0100

    norme string

--- v2/srcs/ft_string.c
+++ v2/srcs/ft_string.c
@@ -6,7 +6,7 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 19:18:21 by jacher            #+#    #+#             */
-/*   Updated: 2020/12/09 18:12:09 by jacher           ###   ########.fr       */
+/*   Updated: 2020/12/09 18:14:14 by jacher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,15 +84,7 @@ void	display_string(char *tmp, int *res, char *s, int bol)
 	*res = ft_strlen(tmp);
 	free(tmp);
 	if (bol == 1)
-		free(s);
-}
-
-void	string_null(char *s, int *bol, flag_list *flags)
-{
-	s = ft_strdup("(null)");
-	*bol = 1;
-	if (flags->b_width == 0 && flags->b_star_length == 1)
-		flags->b_precision = 0;
+		free(s); 
 }
 
 int				ft_string(va_list args, int *res, flag_list *flags)
@@ -108,14 +100,12 @@ int				ft_string(va_list args, int *res, flag_list *flags)
 		return ((*res = -1));
 	s = va_arg(args, char *);
 	if (s == NULL)
-		string_null(s, &bol, flags);
-	/*
 	{
 		s = ft_strdup("(null)");
 		bol = 1;
 		if (flags->b_width == 0 && flags->b_star_length == 1)
 			flags->b_precision = 0;
-	}*/
+	}
 	string_help(flags, &width, &length, s);
 	if (!(tmp = malloc(sizeof(char) * (width + 1))))
 	{
