2c7829fc15199137f2ce61b5d7d8ba417101ff0d
joann8 <joann.acher8@gmail.com>
2020-12-12 22:11:46 +0100

    add

--- v5/srcs/ft_add.c
+++ v5/srcs/ft_add.c
@@ -6,16 +6,15 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 14:28:47 by jacher            #+#    #+#             */
-/*   Updated: 2020/12/12 22:09:46 by jacher           ###   ########.fr       */
+/*   Updated: 2020/12/12 22:11:41 by jacher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pf.h"
 
-void			ft_add_right(char *tmp, char *tmp1, unsigned int width)
+void			ft_add_right(char *tmp, unsigned int width)
 {
 	unsigned int i;
-	(void)tmp1;
 
 	i = 0;
 	while (i < width - ft_strlen(tmp))
@@ -26,11 +25,10 @@ void			ft_add_right(char *tmp, char *tmp1, unsigned int width)
 	ft_putstr(tmp);
 }
 
-void			ft_add_left(char *tmp, char *tmp1, unsigned int width)
+void			ft_add_left(char *tmp, unsigned int width)
 {
 	unsigned int i;
 
-	(void)tmp1;
 	i = 0;
 	ft_putstr(tmp);
 	i = ft_strlen(tmp) - 1;
@@ -40,14 +38,14 @@ void			ft_add_left(char *tmp, char *tmp1, unsigned int width)
 		i++;
 	}
 }
-
+/*
 void			display_add(char *tmp1, int *res, char *tmp)
 {
 	ft_putstr(tmp1);
 	*res += ft_strlen(tmp1);
 	free(tmp1);
 	free(tmp);
-}
+}*/
 
 int				ft_p(va_list args, int *res, t_flag *flags)
 {
@@ -72,6 +70,7 @@ int				ft_p(va_list args, int *res, t_flag *flags)
 		ft_add_right(tmp, tmp1, width);
 	else
 		ft_add_left(tmp, tmp1, width);
-	display_add(tmp1, res, tmp);
+	free(tmp);
+//	display_add(tmp1, res, tmp);
 	return (1);
 }
