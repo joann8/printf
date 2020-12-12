aeaab49df8e32dee9313e5784d204cb5572ade5a
joann8 <joann.acher8@gmail.com>
2020-12-12 22:09:56 +0100

    add

--- v5/srcs/ft_add.c
+++ v5/srcs/ft_add.c
@@ -6,15 +6,16 @@
 /*   By: jacher <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 14:28:47 by jacher            #+#    #+#             */
-/*   Updated: 2020/12/12 22:01:22 by jacher           ###   ########.fr       */
+/*   Updated: 2020/12/12 22:09:46 by jacher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pf.h"
 
-void			ft_add_right(char *tmp, unsigned int width)
+void			ft_add_right(char *tmp, char *tmp1, unsigned int width)
 {
 	unsigned int i;
+	(void)tmp1;
 
 	i = 0;
 	while (i < width - ft_strlen(tmp))
@@ -23,15 +24,16 @@ void			ft_add_right(char *tmp, unsigned int width)
 		i++;
 	}
 	ft_putstr(tmp);
-	free(tmp);
 }
 
-void			ft_add_left(char *tmp, unsigned int width)
+void			ft_add_left(char *tmp, char *tmp1, unsigned int width)
 {
 	unsigned int i;
 
-	i = ft_strlen(tmp) - 1;
+	(void)tmp1;
+	i = 0;
 	ft_putstr(tmp);
+	i = ft_strlen(tmp) - 1;
 	while (i < width)
 	{
 		write(1, " ", 1);
@@ -39,10 +41,19 @@ void			ft_add_left(char *tmp, unsigned int width)
 	}
 }
 
+void			display_add(char *tmp1, int *res, char *tmp)
+{
+	ft_putstr(tmp1);
+	*res += ft_strlen(tmp1);
+	free(tmp1);
+	free(tmp);
+}
+
 int				ft_p(va_list args, int *res, t_flag *flags)
 {
 	void			*ptr;
 	char			*tmp;
+	char			*tmp1;
 	unsigned int	width;
 
 	ptr = va_arg(args, void*);
@@ -51,11 +62,16 @@ int				ft_p(va_list args, int *res, t_flag *flags)
 	width = ft_strlen(tmp);
 	if (flags->b_width == 1 && width < flags->v_width)
 		width = flags->v_width;
+	if (!(tmp1 = malloc(sizeof(char) * (width + 1))))
+	{
+		free(tmp);
+		return ((*res = -1));
+	}
 	*res += width;
 	if (flags->b_flag_minus == 0)
-		ft_add_right(tmp, width);
+		ft_add_right(tmp, tmp1, width);
 	else
-		ft_add_left(tmp, width);
-	free(tmp);
+		ft_add_left(tmp, tmp1, width);
+	display_add(tmp1, res, tmp);
 	return (1);
 }
